03adfc440c683a54b7801f345d4b579e52caafe3
dzemal9017989 <dzemal.nikocevic@gmail.com>
2024-06-19 19:35:01 +0200

    Init

---
+++ storage/framework/views/5cb3ffbc611311c45e35aac973cd9f19.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leeftijd Schatting</title>
+</head>
+<body>
+    <form action="/age" method="GET">
+        <label for="name">Voer een naam in:</label>
+        <input type="text" name="name" id="name" required>
+        <button type="submit">Schat leeftijd</button>
+    </form>
+</body>
+</html>
+<?php /**PATH C:\xampp4\htdocs\PHP-laravel-API-agify.io\resources\views/age/form.blade.php ENDPATH**/ ?>
