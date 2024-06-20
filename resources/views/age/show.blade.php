b37ca51329dff7aa69fac1b6f8e91aaa83ecd8a3
dzemal9017989 <dzemal.nikocevic@gmail.com>
2024-06-20 15:05:20 +0200

    Gebruik van CSS is klaar

--- resources/views/age/show.blade.php
+++ resources/views/age/show.blade.php
@@ -1,9 +1,64 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+body {
+        background-color: aqua;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+
+   ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: red;
+          width: 100%;
+          position: fixed;
+          top: 0;
+      }
+
+      li {
+          float: left;
+      }
+
+  li a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+      }
+
+      li a.active:hover {
+          background-color: lightblue;
+      }
+
+      .form-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 60px; /* Adjust this value if needed */
+      }
+
+      .form-container label,
+      .form-container input,
+      .form-container button {
+          margin: 10px 0;
+      }
+    </style>
     <title>Leeftijd Schatting</title>
 </head>
 <body>
+<ul>
+  <li><a href="#dzemal">Dzemal Nikocevic</a></li>
+  <li style="float:right"><a class="active" href="#about">Over ons</a></li>
+</ul>
     <p>De geschatte leeftijd voor {{ $ageData['name'] }} is {{ $ageData['age'] }} jaar.</p>
     <a href="/">Terug</a>
 </body>
--- storage/framework/views/5cb3ffbc611311c45e35aac973cd9f19.php
+++ storage/framework/views/5cb3ffbc611311c45e35aac973cd9f19.php
@@ -2,13 +2,92 @@
 <html>
 <head>
     <title>Leeftijd Schatting</title>
+    <style>
+    body {
+        background-color: aqua;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: red;
+        width: 100%;
+        position: fixed;
+        top: 0;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a.active:hover {
+        background-color: lightblue;
+    }
+
+    .form-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 60px; /* Adjust this value if needed */
+        width: 100%;
+    }
+
+    .form-container form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 300px; /* Adjust this value if needed */
+    }
+
+    .form-container label,
+    .form-container input,
+    .form-container button {
+        margin: 10px 0;
+        width: 100%;
+    }
+
+    .form-container button {
+        background-color: red;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    .form-container button:hover {
+        background-color: darkred;
+    }
+    </style>
 </head>
 <body>
-    <form action="/age" method="GET">
-        <label for="name">Voer een naam in:</label>
-        <input type="text" name="name" id="name" required>
-        <button type="submit">Schat leeftijd</button>
-    </form>
+<ul>
+  <li><a href="#dzemal">Dzemal Nikocevic</a></li>
+  <li style="float:right"><a class="active" href="#about">Over ons</a></li>
+</ul>
+    <div class="form-container">
+        <form action="/age" method="GET">
+            <label for="name"><h1>Voer een naam in:</h1></label>
+            <input type="text" name="name" id="name" required>
+            <button type="submit" class="button">Schat leeftijd</button>
+        </form>
+    </div>
 </body>
 </html>
 <?php /**PATH C:\xampp4\htdocs\PHP-laravel-API-agify.io\resources\views/age/form.blade.php ENDPATH**/ ?>
